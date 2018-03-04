76e629fc3df30cc254a2276f7a41639ccee6170e
atolle - Andrew Tolle <atolle@outlook.com>
2018-03-04 16:07:26 -0600

    Finished front-end changes for contact form.

--- 3PalmsSecurity/Controllers/HomeController.cs
+++ 3PalmsSecurity/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using _3PalmsSecurity.Models;
+using _3PalmsSecurity.ViewModels;
+using System.Net.Mail;
 
 namespace _3PalmsSecurity.Controllers
 {
@@ -32,6 +34,45 @@ namespace _3PalmsSecurity.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactUsViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MailMessage msz = new MailMessage();
+                    msz.From = new MailAddress(vm.Email);//Email which you are getting 
+                                                         //from contact us page 
+                    msz.To.Add("emailaddrss@gmail.com");//Where mail will be sent 
+                    msz.Subject = vm.Company;
+                    msz.Body = "Message body";
+                    SmtpClient smtp = new SmtpClient();
+
+                    smtp.Host = "smtp.gmail.com";
+
+                    smtp.Port = 587;
+
+                    smtp.Credentials = new System.Net.NetworkCredential
+                    ("youremailid@gmail.com", "password");
+
+                    smtp.EnableSsl = true;
+
+                    smtp.Send(msz);
+
+                    ModelState.Clear();
+                    ViewBag.Message = "Thank you for Contacting us ";
+                }
+                catch (Exception ex)
+                {
+                    ModelState.Clear();
+                    ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+                }
+            }
+
+            return View();
         }
     }
 }
---
+++ 3PalmsSecurity/ViewModels/ContactUsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _3PalmsSecurity.ViewModels
+{
+    public class ContactUsViewModel
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; }
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Please enter a valid phone number.")]
+        public string Phone { get; set; }
+    }
+}
